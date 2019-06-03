9c33b5cb788b5820594b1f471b12bf356c914a8b
Chenbo Yi <yichenbo@outlook.com>
2019-06-03 20:58:33 +0100

    add feature tests for printing bank statement

---
+++ spec/FeatureTests/print_statement_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'bank'
+
+describe 'print the Bank Statment' do
+  let(:account) { Bank.new }
+
+  xit 'print the bank statment with transaction history in reverse order' do
+    date = Time.now.strftime('%d/%m/%Y')
+    account.deposit(100.00)
+    account.withdraw(20.00)
+    expect(account.print_statment).to eq
+    "date || credit || debit || balance\n
+    #{date} || || 20.00 || 80.00\n
+    #{date} || 100.00 || || 100.00"
+  end
+
+  xit 'print the bank statment with transaction on different days' do
+    allow(Time).to receive(:now).and_return(Date.parse('10-01-2012'))
+    date1 = Time.now.strftime('%d/%m/%Y')
+    account.deposit(1000.00)
+    allow(Time).to receive(:now).and_return(Date.parse('13-01-2012'))
+    date2 = Time.now.strftime('%d/%m/%Y')
+    account.deposit(2000.00)
+    allow(Time).to receive(:now).and_return(Date.parse('14-01-2012'))
+    date3 = Time.now.strftime('%d/%m/%Y')
+    account.withdraw(500.00)
+    expect(account.print_statment).to eq
+    "date || credit || debit || balance\n
+    #{date3} || || 500.00 || 2500.00\n
+    #{date2} || 2000.00 || || 3000.00\n
+    #{date1} || 1000.00 || || 1000.00"
+  end
+end
